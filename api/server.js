f6f697e3019274a9fdeb9af63e507d92d049843d
Tate Keller <tatek1993@gmail.com>
2020-03-19 18:11:52 -0500

    server is running

--- api/server.js
+++ api/server.js
@@ -6,12 +6,20 @@ const helmet = require('helmet');
 const CORS = require('cors');
 
 // bring in the router
-const characterRouter = require('../characters/charactersRouter.js');
+// const characterRouter = require('../characters/charactersRouter.js');
 
 //create the server
 const server = express();
 
 // "teach" the server to read JSON
 server.use(helmet());
-server.use(CORS);
+server.use(CORS());
 server.use(express.json());
+
+// server.use('/api/router', characterRouter)
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Hello World</h1>`)
+})
+
+module.exports = server;
